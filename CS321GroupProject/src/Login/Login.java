79af0006949b6e6aa0e0e81b361fed518035bec2
Nick <nicholas.s.burns17@gmail.com>
2018-10-25 16:16:11 -0500

    Fixed RegisteredLogin.txt file

--- CS321GroupProject/src/Login/Login.java
+++ CS321GroupProject/src/Login/Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package login;
+package Login;
 
 import java.util.*;
 import java.io.FileNotFoundException;
