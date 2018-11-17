654757d35e2832364525ea6dfafa10d12e5c4acb
Nick <nicholas.s.burns17@gmail.com>
2018-11-17 14:12:02 -0600

    Update User.java

--- CS321GroupProject/src/Model/User.java
+++ CS321GroupProject/src/Model/User.java
@@ -9,94 +9,219 @@ import java.util.*;
 
 public class User
 {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+    private String birthday;
+    private String gameInterest;
+    private int level;
+    private int messagesSent;
+    
+    //
+    //
+    //Class constructors
+    //
+    //
+    
     /**
-      Creates UserData object.
-      @param aUsername username string
-      @param aPassword password string
-      @param aGameInterest game title string
-      @param aAge age string
-   */
-    public User(String aUsername, String aPassword, String aGameInterest, String aAge)
+     * User constructor for a registerController
+     * @param firstname
+     * @param lastname
+     * @param username
+     * @param password
+     * @param birthday
+     * @param aGameInterest 
+     */
+    public User(String firstname, String lastname, String username, String password, String birthday, String aGameInterest, int messagesSent)
     {
-       username = aUsername;
-       password = aPassword;
-       gameInterest = aGameInterest;
-       age = aAge;
+       this.firstname = firstname;
+       this.lastname = lastname;
+       this.username = username;
+       this.password = password;
+       this.birthday = birthday;
+       this.gameInterest = gameInterest;
+       this.messagesSent = messagesSent;
     }
     
     /**
-      Set username
-      @param newUsername the new password
-   */
-   public void setUsername(String newUsername)
-   {
-      password = newUsername;
-   }
+     * User constructor for a loginController
+     * @param username
+     * @param password 
+     */
+    public User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+    
+    /**
+     * User constructor for a HomePageController
+     * @param username
+     * @param birthday
+     * @param gameInterest 
+     */
+    public User(String username, String birthday, String gameInterest)
+    {
+        this.username = username;
+        this.birthday = birthday;
+        this.gameInterest = gameInterest;
+    }
+    
+    //
+    //Class get and set methods
+    //
    
    /**
-      Get username.
-   */
+    * Returns the Username
+    * @return 
+    */
    public String getUsername()
    {
       return username;
    }
    
    /**
-      Set password
-      @param newPassword the new password
-   */
+    * Set the username
+    * @param username 
+    */
+   public void setUsername(String username)
+   {
+       this.username = username;
+   }
+   
+   /**
+    * Returns the User password
+    * @return 
+    */
+   public String getPassword()
+   {
+       return password;
+   }
+   
+   /**
+    * Sets the password
+    * @param newPassword 
+    */
    public void setPassword(String newPassword)
    {
       password = newPassword;
    }
-
-    /**
-      Set gameInterest
-      @param newGameInterest the new game interest title
-   */
-   public void setGameInterest(String newGameInterest)
+   
+   /**
+    * Returns the firstname
+    * @return 
+    */
+   public String getFirstName()
    {
-      gameInterest = newGameInterest;
+       return firstname;
    }
    
    /**
-      Get gameInterest.
-   */
+    * Sets the firstname
+    * @param firstname 
+    */
+   public void setFirstName(String firstname)
+   {
+       this.firstname = firstname;
+   }
+   
+   /**
+    * Returns the lastname
+    * @return 
+    */
+   public String getLastName()
+   {
+       return lastname;
+   }
+   
+   /**
+    * Sets the lastname
+    * @param lastname 
+    */
+   public void setLastname(String lastname)
+   {
+        this.lastname = lastname;
+   }
+   
+   /**
+    * Returns the gameInterest
+    * @return 
+    */
    public String getGameInterest()
    {
-      return gameInterest;
-   }
-   
-    /**
-      Set age.
-      @param newAge the new age
-   */
-   public void setAge(String newAge)
-   {
-      age = newAge;
+        return gameInterest;
    }
    
    /**
-      Get age.
-   */
-   public String getAge()
+    * Sets the gameInterest
+    * @param gameInterest 
+    */
+   public void setGameInterest(String gameInterest)
    {
-      return age;
-   }
-    
-   /**
-   Check if the password is correct.
-   @param aPassword a password to check
-   @return true if the supplied password matches the user entered password.
-   */
-   public boolean checkPassword(String aPassword)
-   {
-      return aPassword.equals(password);
+        this.gameInterest = gameInterest;
    }
    
-    private String username;
-    private String password;
-    private String gameInterest;
-    private String age;
-    
+   /**
+    * Returns birthday
+    * @return 
+    */
+   public String getBirthday()
+   {
+        return birthday;
+   }
+   
+   /**
+    * Sets the birthday
+    * @return 
+    */
+   public void setBirthday(String birthday)
+   {
+        this.birthday = birthday;
+   }
+   
+   /**
+    * Returns the level
+    * @return 
+    */
+   public int getLevel()
+   {
+       this.calculateLevel();
+       return level;
+   }
+   
+   /**
+    * Returns the level
+    * @param level 
+    */
+   public void setLevel(int level)
+   {
+       this.level = level;
+   }
+   
+   /**
+    * calculates the level based on messagesSent
+    */
+   private void calculateLevel()
+   {
+       level = (messagesSent/5);
+   }
+   
+   /**
+    * Returns the messagesSent
+    * @return 
+    */
+   public int getMessagesSent()
+   {
+       return messagesSent;
+   }
+   
+   /**
+    * Sets the messagesSent
+    * @param messagesSent 
+    */
+   public void setMessagesSent(int messagesSent)
+   {
+       this.messagesSent = messagesSent;
+   }
 }
